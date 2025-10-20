1ac3af7019e725bef8b34ca5abd79f72b8c7fad9
User <User@WIN-1BK9C7H7MKE>
2025-10-20 21:42:39 +0300

    Добавьте файлы проекта.

---
+++ OOP-1/Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_1
+{
+    internal class Person
+    {
+        private string name;
+        private int age;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+
+        }
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value >= 0 && value <= 120)
+                    age = value;
+                else
+                {
+                    Console.WriteLine("Возраст должен быть от 0 до 120!");
+                }
+            }
+        }
+
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+                
+        }
+        public void SayHello()
+        {
+            Console.WriteLine($"Привет, я {name} мне {age} лет.");
+
+        }
+    }
+}
